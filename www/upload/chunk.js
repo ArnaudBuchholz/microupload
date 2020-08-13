95d5d6bc474c5389664111a264c4dd08f0e79d9c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-13 16:13:40 -0400

    chunk

---
+++ www/upload/chunk.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { chunk } = require('../../uploads')
+
+module.exports = async (request, response) => {
+  const id = request.headers['x-upload-id']
+  const offset = parseInt(request.headers['x-upload-offset'] || '0', 10)
+  await chunk(id, offset, request)
+  response.writeHead(200, {
+    'content-type': 'application/json'
+  })
+  response.end('{}')
+}
