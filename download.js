278580267648d15ba2551005f69b1457e30a27a7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-15 00:03:56 -0400

    download

---
+++ download.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { promisify } = require('util')
+const { join } = require('path')
+const pipeline = promisify(require('stream').pipeline)
+const { createReadStream } = require('fs')
+const { decrypt } = require('kaos')
+  
+module.exports = (id, literalKey, response) => pipeline(
+  createReadStream(join(__dirname, 'storage', id)),
+  decrypt(literalKey),
+  response
+)
