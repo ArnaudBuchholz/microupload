2e02afc99cc8e30dc72dc3f83bf10d5a3d487426
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-17 00:18:59 -0400

    Linter fix

--- download.js
+++ download.js
@@ -5,7 +5,7 @@ const { join } = require('path')
 const pipeline = promisify(require('stream').pipeline)
 const { createReadStream } = require('fs')
 const { decrypt } = require('kaos')
-  
+
 module.exports = (id, literalKey, response) => pipeline(
   createReadStream(join(__dirname, 'storage', id)),
   decrypt(literalKey),
