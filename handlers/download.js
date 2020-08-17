9e856cb1dfc7b730c3d4b6311c18064e4a002cbf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-17 00:18:51 -0400

    Moves & finalizes handler

--- handlers/download.js
+++ handlers/download.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const body = require('../node_modules/reserve/body.js') // https://github.com/ArnaudBuchholz/reserve/issues/16
+const body = require('../node_modules/reserve/body') // https://github.com/ArnaudBuchholz/reserve/issues/16
 const download = require('../download')
 
 module.exports = async (request, response) => {
@@ -8,6 +8,7 @@ module.exports = async (request, response) => {
   const key = params[1]
   const id = params[2]
   response.writeHead(200, {
+    'content-disposition': `attachment; filename=${id}`,
     'content-type': 'application/octet-stream'
   })
   return download(id, key, response)
