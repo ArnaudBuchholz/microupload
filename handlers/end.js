a4b80e0bf7202b99329c6dd5fea3510fb60100d8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-08-20 22:15:16 -0400

    Simplify body access

--- handlers/end.js
+++ handlers/end.js
@@ -1,7 +1,9 @@
 'use strict'
 
-const body = require('../node_modules/reserve/body') // https://github.com/ArnaudBuchholz/reserve/issues/16
+const body = require('reserve/body')
 const { end } = require('../uploads')
+
+console.log(body)
 
 module.exports = async (request, response) => {
   await end(await body(request).id)
