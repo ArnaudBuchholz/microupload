a8ed145d99bef3eb434e584e72493321edf835ce
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-04 23:47:13 -0400

    Use new storage

--- download.js
+++ download.js
@@ -5,9 +5,10 @@ const { join } = require('path')
 const pipeline = promisify(require('stream').pipeline)
 const { createReadStream } = require('fs')
 const { decrypt } = require('kaos')
+const storage = require('./storage')
 
 module.exports = (id, literalKey, response) => pipeline(
-  createReadStream(join(__dirname, 'storage', id)),
+  createReadStream(join(storage, id)),
   decrypt(literalKey),
   response
 )
