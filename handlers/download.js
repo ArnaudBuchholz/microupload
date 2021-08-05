59eab85170ef1401756d0ef327aaaf04045ac62b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-04 23:47:45 -0400

    Enables filename

--- handlers/download.js
+++ handlers/download.js
@@ -2,14 +2,16 @@
 
 const body = require('reserve/body')
 const download = require('../download')
+const mime = require('mime')
 
 module.exports = async (request, response) => {
-  const params = /key=([^&]*)&id=(.*)/.exec(await body(request))
-  const key = params[1]
-  const id = params[2]
+  const parameters = await body(request)
+  const key = /key=([^&]*)/.exec(parameters)[1]
+  const id = /id=([^&]*)/.exec(parameters)[1]
+  const filename = /filename=([^&]*)/.exec(parameters)[1]
   response.writeHead(200, {
-    'content-disposition': `attachment; filename=${id}`,
-    'content-type': 'application/octet-stream'
+    'content-disposition': `attachment; filename=${filename}`,
+    'content-type': mime.getType(filename) || 'application/octet-stream'
   })
   return download(id, key, response)
 }
