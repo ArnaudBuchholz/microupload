f7a47a16a6fff1bb4cff4683cb6d4094470bb566
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-08-22 21:54:05 -0400

    Use sendJSON

--- handlers/chunk.js
+++ handlers/chunk.js
@@ -1,13 +1,11 @@
 'use strict'
 
 const { chunk } = require('../uploads')
+const sendJSON = require('./sendJSON')
 
 module.exports = async (request, response) => {
   const id = request.headers['x-upload-id']
   const offset = parseInt(request.headers['x-upload-offset'] || '0', 10)
   await chunk(id, offset, request)
-  response.writeHead(200, {
-    'content-type': 'application/json'
-  })
-  response.end('{}')
+  sendJSON(response, {})
 }
