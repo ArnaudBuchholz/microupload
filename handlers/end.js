91d9a6fb9010301638133a584551b202058a542c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-08-22 21:54:13 -0400

    Use sendJSON

--- handlers/end.js
+++ handlers/end.js
@@ -2,11 +2,9 @@
 
 const body = require('reserve/body')
 const { end } = require('../uploads')
+const sendJSON = require('./sendJSON')
 
 module.exports = async (request, response) => {
   await end(await body(request).id)
-  response.writeHead(200, {
-    'content-type': 'application/json'
-  })
-  response.end('{}')
+  sendJSON(response, {})
 }
